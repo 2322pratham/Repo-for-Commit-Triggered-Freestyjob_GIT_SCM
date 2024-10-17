f7797d1188bb11db47f1b981890806e5e8bae68e
PRATHAM PRAKASH KAMBLE <mca.2322@unigoa.ac.in>
2024-10-17 16:13:10 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -2,5 +2,6 @@ public class Pattern2 {
     public static void main(String[] args) {
         System.out.println("hello i have created one more file ");
         System.out.println("one more line added to the program ");
+        System.out.println("program lineadded ");
     }
 }
